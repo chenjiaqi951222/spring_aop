84dd8bde2a9e6801cee1121629e4236a1e15bd5b
chenjiaqi <820626324@qq.com>
2018-12-04 09:48:06 +0800

    Initial commit

---
+++ src/main/java/com/imooc/aop/demo2/SpringDemo2.java
@@ -0,0 +1,16 @@
+package com.imooc.aop.demo2;
+
+import org.junit.Test;
+
+public class SpringDemo2 {
+    @Test
+    public void demo1() {
+        ProductDao productDao = new ProductDao();
+
+        ProductDao proxy = (ProductDao) new MyCglibProxy(productDao).createProxy();
+        proxy.delete();
+        proxy.find();
+        proxy.save();
+        proxy.find();
+    }
+}
---
+++ src/main/java/com/imooc/aop/demo6/SpringDemo6.java
@@ -0,0 +1,31 @@
+package com.imooc.aop.demo6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext4.xml")
+public class SpringDemo6 {
+    @Resource(name = "studentDao")
+    private StudentDao studentDao;
+    @Resource(name = "customerDao")
+    private CustomerDao customerDao;
+
+    @Test
+    public void demo1() {
+        studentDao.find();
+        studentDao.delete();
+        studentDao.save();
+        studentDao.update();
+
+        customerDao.find();
+        customerDao.save();
+        customerDao.update();
+        customerDao.delete();
+    }
+
+}
